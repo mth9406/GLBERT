b234d37aa7a2b4175818235f118463898cca2465
mth9406 <mth9406@naver.com>
2022-04-16 23:19:43 +0900

    modified GLBERT

--- pytorch_code/model.py
+++ pytorch_code/model.py
@@ -7,92 +7,11 @@ Created on July, 2018
 """
 
 import datetime
-import math
 import numpy as np
 import torch
 from torch import nn
-from torch.nn import Module, Parameter
 import torch.nn.functional as F
 from layers import * 
-
-class GNN(Module):
-    def __init__(self, hidden_size, step=1):
-        super(GNN, self).__init__()
-        self.step = step
-        self.hidden_size = hidden_size
-        self.input_size = hidden_size * 2
-        self.gate_size = 3 * hidden_size
-        self.w_ih = Parameter(torch.Tensor(self.gate_size, self.input_size))
-        self.w_hh = Parameter(torch.Tensor(self.gate_size, self.hidden_size))
-        self.b_ih = Parameter(torch.Tensor(self.gate_size))
-        self.b_hh = Parameter(torch.Tensor(self.gate_size))
-        self.b_iah = Parameter(torch.Tensor(self.hidden_size))
-        self.b_oah = Parameter(torch.Tensor(self.hidden_size))
-
-        self.linear_edge_in = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_out = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_f = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-
-    def GNNCell(self, A, hidden):
-        input_in = torch.matmul(A[:, :, :A.shape[1]], self.linear_edge_in(hidden)) + self.b_iah
-        input_out = torch.matmul(A[:, :, A.shape[1]: 2 * A.shape[1]], self.linear_edge_out(hidden)) + self.b_oah
-        inputs = torch.cat([input_in, input_out], 2)
-        gi = F.linear(inputs, self.w_ih, self.b_ih)
-        gh = F.linear(hidden, self.w_hh, self.b_hh)
-        i_r, i_i, i_n = gi.chunk(3, 2)
-        h_r, h_i, h_n = gh.chunk(3, 2)
-        resetgate = torch.sigmoid(i_r + h_r)
-        inputgate = torch.sigmoid(i_i + h_i)
-        newgate = torch.tanh(i_n + resetgate * h_n)
-        hy = newgate + inputgate * (hidden - newgate)
-        return hy
-
-    def forward(self, A, hidden):
-        for i in range(self.step):
-            hidden = self.GNNCell(A, hidden)
-        return hidden
-
-
-class SessionGraph(Module):
-    def __init__(self, opt, n_node):
-        super(SessionGraph, self).__init__()
-        self.hidden_size = opt.hiddenSize
-        self.n_node = n_node
-        self.batch_size = opt.batchSize
-        self.nonhybrid = opt.nonhybrid
-        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
-        self.gnn = GNN(self.hidden_size, step=opt.step)
-        self.linear_one = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_two = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
-        self.linear_three = nn.Linear(self.hidden_size, 1, bias=False)
-        self.linear_transform = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=True)
-        self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        stdv = 1.0 / math.sqrt(self.hidden_size)
-        for weight in self.parameters():
-            weight.data.uniform_(-stdv, stdv)
-
-    def compute_scores(self, hidden, mask):
-        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
-        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
-        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
-        alpha = self.linear_three(torch.sigmoid(q1 + q2))
-        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
-        if not self.nonhybrid:
-            a = self.linear_transform(torch.cat([a, ht], 1))
-        b = self.embedding.weight[1:]  # n_nodes x latent_size
-        scores = torch.matmul(a, b.transpose(1, 0))
-        return scores
-
-    def forward(self, inputs, A):
-        hidden = self.embedding(inputs)
-        hidden = self.gnn(A, hidden)
-        return hidden
-
 
 class GLBert4Rec(nn.Module):
 
@@ -118,11 +37,10 @@ class GLBert4Rec(nn.Module):
         self.enc_layers = nn.ModuleList(
             [EncoderLayer(opt.hidden_dim, opt.num_head, opt.inner_dim) for _ in range(opt.N)]
         )
-        self.linear_one = nn.Linear(self.hidden_dim, self.hidden_dim, bias=True)
-        self.linear_two = nn.Linear(self.hidden_dim, self.hidden_dim, bias=True)
-        self.linear_three = nn.Linear(self.hidden_dim, 1, bias=False)
-        self.linear_transform = nn.Linear(self.hidden_dim * 2, self.hidden_dim, bias=True)
-        self.nonhybrid = opt.nonhybrid
+        self.projection = nn.Sequential(
+            nn.Linear(opt.hidden_dim, opt.hidden_dim),
+            nn.Linear(opt.hidden_dim, n_items)
+        )
         self.dropout = nn.Dropout(0.1)
 
         self.loss_function = nn.CrossEntropyLoss()
@@ -149,22 +67,14 @@ class GLBert4Rec(nn.Module):
             output_out = torch.matmul(A[:, :, A.shape[1]:2*A.shape[1]], graph_out_conv_layer(output))
             output = graph_in_out_mix_conv_layer(torch.cat([output_in, output_out], 2))
             output = enc_layer(output, mask)
-        # (bs, item_len, 2*hidden_dim)
-        # output = output[:, -1, :] # (bs, hidden_dim)
-        # output = self.projection(output)
+        # (bs, item_len, hidden_dim)
+        output = output[:, -1, :] # (bs, hidden_dim)
+        output = self.projection(output)
         return output
 
-    def compute_scores(self, hidden, mask):
-        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
-        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
-        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
-        alpha = self.linear_three(torch.sigmoid(q1 + q2))
-        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
-        if not self.nonhybrid:
-            a = self.linear_transform(torch.cat([a, ht], 1))
-        b = self.embedding.weight[1:]  # n_nodes x latent_size
-        scores = torch.matmul(a, b.transpose(1, 0))
-        return scores
+    # def compute_scores(self, hidden, mask):
+    #     scores =  self.projection(hidden[:, -1, :])
+    #     return scores
 
 def trans_to_cuda(variable):
     if torch.cuda.is_available():
@@ -187,10 +97,9 @@ def forward(model, i, data):
     A = trans_to_cuda(torch.Tensor(A).float())
     mask = trans_to_cuda(torch.Tensor(mask).long())
     hidden = model(items, A)
-    get = lambda i: hidden[i][alias_inputs[i]]
-    seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
-    return targets, model.compute_scores(seq_hidden, mask)
-
+    # get = lambda i: hidden[i][alias_inputs[i]]
+    # seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
+    return targets, hidden
 
 def train_test(model, train_data, test_data):
     model.scheduler.step()
@@ -202,7 +111,7 @@ def train_test(model, train_data, test_data):
         model.optimizer.zero_grad()
         targets, scores = forward(model, i, train_data)
         targets = trans_to_cuda(torch.Tensor(targets).long())
-        loss = model.loss_function(scores, targets - 1)
+        loss = model.loss_function(scores, targets)
         loss.backward()
         model.optimizer.step()
         total_loss += loss
@@ -219,11 +128,11 @@ def train_test(model, train_data, test_data):
         sub_scores = scores.topk(20)[1]
         sub_scores = trans_to_cpu(sub_scores).detach().numpy()
         for score, target, mask in zip(sub_scores, targets, test_data.mask):
-            hit.append(np.isin(target - 1, score))
-            if len(np.where(score == target - 1)[0]) == 0:
+            hit.append(np.isin(target, score))
+            if len(np.where(score == target)[0]) == 0:
                 mrr.append(0)
             else:
-                mrr.append(1 / (np.where(score == target - 1)[0][0] + 1))
+                mrr.append(1 / (np.where(score == target)[0][0] + 1))
     hit = np.mean(hit) * 100
     mrr = np.mean(mrr) * 100
     return hit, mrr
